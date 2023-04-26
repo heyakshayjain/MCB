f401e2accf00c30203951b0270a15884ad42c429
Akshay Jain <114152892+heyakshayjain@users.noreply.github.com>
2023-04-26 16:19:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ $.ajax({
   dataType: 'text',
 }).done(function(data) {
   // Parse CSV data into an array of objects
+  var careerOptions = $.csv.toObjects(data);
+  
+  // Function to display all career options
+  function displayCareerOptions() {
+    var careerOptionsList = $('#careerOptionsList');
+    careerOptionsList.empty();
+    for (var i = 0; i < careerOptions.length; i++) {
+      var careerOption = careerOptions[i];
+      var listItem = $('<li>').text(careerOption.CareerOption);
+      careerOptionsList.append(listItem);
+    }
+  }
+
+  // Display all career options initially
+  displayCareerOptions();
+  
+  // Button click event to display all career options
+  $('#showAllBtn').on('click', function() {
+    displayCareerOptions();
+  });
+
+}).fail(function(jqXHR, textStatus, errorThrown) {
+  console.error('Failed to fetch data from Google Sheet:', errorThrown);
+});
